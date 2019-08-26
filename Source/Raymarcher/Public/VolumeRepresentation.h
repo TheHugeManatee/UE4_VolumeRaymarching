7c3c8c7d39e9b137e891c63dc26c283e1aa0811f
Jakob Weiss <jakob.weiss@tum.de>
2019-08-26 11:54:31 +0200

    minor cleaup/comments

--- Source/Raymarcher/Public/VolumeRepresentation.h
+++ Source/Raymarcher/Public/VolumeRepresentation.h
@@ -17,15 +17,37 @@ public:
   UVolumeRepresentation();
   ~UVolumeRepresentation();
 
+  /**
+   * The volume texture that contains the actual data.
+   */
   UPROPERTY(BlueprintReadWrite, EditAnywhere)
   UVolumeTexture* Texture;
 
+  /**
+   * The volume size in MM.
+   */
   UPROPERTY(BlueprintReadWrite, EditAnywhere)
   FVector VolumeSizeInMM;
 
+  /**
+   * The location of the volume center in the world.
+   */
   UPROPERTY(BlueprintReadWrite, EditAnywhere)
   FVector CenterLocation;
 
+  /**
+   * The voxel dimensions of the volume.
+   */
   UPROPERTY(BlueprintReadWrite, EditAnywhere)
   FIntVector Dimensions;
+
+  /**
+   * Tags associated with this volume.
+   * This can be used to indicate modality, which post-processing operations were applied etc..
+   * Common tags are:
+   *
+   *
+   */
+  UPROPERTY(BlueprintReadWrite, EditAnywhere)
+  TMap<FString, FString> Tags;
 };
